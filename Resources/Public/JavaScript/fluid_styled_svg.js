3fe902fc210f43f24ccc72364d906cbe3cc38d67
Manuel Munz <munz@comuno.net>
2017-09-24 17:12:20 +0200

    fix inlining via js by using a scopepreserver function

--- Resources/Public/JavaScript/fluid_styled_svg.js
+++ Resources/Public/JavaScript/fluid_styled_svg.js
@@ -1,26 +1,36 @@
 // replace all svg object tags with inline svg
 
-function injectSvg(target) {
-    var xmlDoc = target.contentDocument.documentElement;
-    var svg = $(xmlDoc)[0];
-    var width = target.getAttribute('width');
-    var height = target.getAttribute('height');
-    var classNames = target.getAttribute('class').replace('c1-svg__image--inject', 'c1-svg__image--injected');
+function scopepreserver(el) {
+    return function () {
+        injectSvg(el)
+    };
+}
 
-    svg.setAttribute('width', '100%');
-    svg.setAttribute('height', '100%');
-    svg.setAttribute('class', classNames);
-    target.parentNode.replaceChild(svg, target);
+function injectSvg(target) {
+    if (target.contentDocument) {
+        var xmlDoc = target.contentDocument.documentElement;
+        var svg = $(xmlDoc)[0];
+        var width = target.getAttribute('width');
+        var height = target.getAttribute('height');
+        var classNames = target.getAttribute('class').replace('c1-svg__image--inject', 'c1-svg__image--injected');
+
+        svg.setAttribute('width', '100%');
+        svg.setAttribute('height', '100%');
+        svg.setAttribute('class', classNames);
+        target.removeEventListener('load', 'inject');
+        target.parentNode.replaceChild(svg, target);
+    } else {
+        throw('target.contentDocument is empty.')
+    }
 };
 
 function svgInline() {
+
     var elements = document.getElementsByClassName("c1-svg__image--inject");
     for(var i=0; i<elements.length; i++)
     {
         var el = elements[i];
-        el.addEventListener('load', function () {
-            injectSvg(el)
-        }, true);
+        el.onload = scopepreserver(el);
         // next line triggers load event in case the resource was already loaded from cache
         // fixes an issue in safari
         el.data = el.data;
