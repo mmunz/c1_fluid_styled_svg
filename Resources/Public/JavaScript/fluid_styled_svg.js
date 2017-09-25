124f9268bb0333ac095dab90d03cefa664e9b7b0
Manuel Munz <munz@comuno.net>
2017-09-25 19:38:59 +0200

    js cleanup and more explanationary comments

--- Resources/Public/JavaScript/fluid_styled_svg.js
+++ Resources/Public/JavaScript/fluid_styled_svg.js
@@ -1,4 +1,15 @@
-// replace all svg object tags with inline svg
+/*
+ Replace all object tags with an SVG as data attribute and class 'c1-svg__image--inject' with inline SVG
+
+ This is done reading the objects content (the loaded svg graph) from inside the object once it is loaded, so no
+ additional request is required for this to work.
+
+ Also some attributes inside the SVG need to be rewritten:
+
+ - height and width set to 100%
+ - classnames are added to the SVG. They copied from the objects class, but with 'inject' replaced by 'injected')
+ */
+
 function scopepreserver(el) {
     return function () {
         injectSvg(el)
@@ -25,18 +36,19 @@ function injectSvg(target) {
 
 function svgInline() {
     var elements = document.getElementsByClassName("c1-svg__image--inject");
-    for(var i=0; i<elements.length; i++)
-    {
+    for (var i = 0; i < elements.length; i++) {
         var el = elements[i];
         el.addEventListener('load', scopepreserver(el));
-        //el.onload = scopepreserver(el);
         // next line triggers load event in case the resource was already loaded from cache
         // fixes an issue in safari
         el.data = el.data;
     }
 }
 
-(function(fn){var d=document;(d.readyState=='loading')?d.addEventListener('DOMContentLoaded',fn):fn();})(function(){
+(function (fn) {
+    var d = document;
+    (d.readyState == 'loading') ? d.addEventListener('DOMContentLoaded', fn) : fn();
+})(function () {
     svgInline();
 });
 
