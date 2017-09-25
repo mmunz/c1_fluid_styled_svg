a2190dee58a25702850b8f655f59af233153cc30
Manuel Munz <munz@comuno.net>
2017-09-25 19:33:07 +0200

    fix removeEventListener call

--- Resources/Public/JavaScript/fluid_styled_svg.js
+++ Resources/Public/JavaScript/fluid_styled_svg.js
@@ -1,5 +1,4 @@
 // replace all svg object tags with inline svg
-
 function scopepreserver(el) {
     return function () {
         injectSvg(el)
@@ -17,7 +16,7 @@ function injectSvg(target) {
         svg.setAttribute('width', '100%');
         svg.setAttribute('height', '100%');
         svg.setAttribute('class', classNames);
-        target.removeEventListener('load', 'inject');
+        target.removeEventListener('load', scopepreserver);
         target.parentNode.replaceChild(svg, target);
     } else {
         throw('target.contentDocument is empty.')
@@ -25,12 +24,12 @@ function injectSvg(target) {
 };
 
 function svgInline() {
-
     var elements = document.getElementsByClassName("c1-svg__image--inject");
     for(var i=0; i<elements.length; i++)
     {
         var el = elements[i];
-        el.onload = scopepreserver(el);
+        el.addEventListener('load', scopepreserver(el));
+        //el.onload = scopepreserver(el);
         // next line triggers load event in case the resource was already loaded from cache
         // fixes an issue in safari
         el.data = el.data;
@@ -40,3 +39,4 @@ function svgInline() {
 (function(fn){var d=document;(d.readyState=='loading')?d.addEventListener('DOMContentLoaded',fn):fn();})(function(){
     svgInline();
 });
+
